36c7afc4cf8990fc0524f5b84b90206f7dcb2623
Arthur-All <arthur.r.t.allen@gmail.com>
2024-02-09 23:48:50 -0800

    make database.js client.connection() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,21 @@ async function query(queryObj) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("credenciais:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObj);
     return result;
-  } catch (ex) {
-    console.error(ex);
+  } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
